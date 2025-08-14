89276be87b761fa94584aa89bec1ed34585b0490
CoderSELIMANE <aitsielmadani@gmail.com>
2025-08-14 16:38:39 +0100

    update settings 5

--- espace_client_app/settings.py
+++ espace_client_app/settings.py
@@ -45,7 +45,17 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
+    'storages',
 ]
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = '0057a9e8241d8490000000001'  # keyID Backblaze
+AWS_SECRET_ACCESS_KEY = 'K005XBcr7UY6enLUmbQydRXYgCGon9k'  # applicationKey Backblaze
+AWS_STORAGE_BUCKET_NAME = 'mes-docs'
+AWS_S3_REGION_NAME = 'us-east-005'  # selon la région donnée par Backblaze
+AWS_S3_ENDPOINT_URL = 'https://s3.us-east-005.backblazeb2.com'  # Endpoint S3 exact
+AWS_QUERYSTRING_AUTH = False
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
